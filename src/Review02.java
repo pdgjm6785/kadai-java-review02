55daab9f5f1f4429d30d0a355d8f74d64a8427fc
pdgjm6785 <pdgjm6785@gmail.com>
2023-11-14 14:32:19 +0900

    docs

--- src/Review02.java
+++ src/Review02.java
@@ -2,7 +2,7 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        // 整数iの値0から100より小さい間、処理を繰り返す
+        // 整数iの値1から100より小さい間、処理を繰り返す
         for (int i = 1; i <= 100; i++) {
             // iの値が３で割り切れるかつ５で割り切れるなら”FizzBuss”と表示
             if (i % 3 == 0 && i % 5 == 0) {
